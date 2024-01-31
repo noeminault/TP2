a8c112c306559e7df32285b203cb2783e20448ec
noeminault <noeminault@gmail.com>
2024-01-31 09:27:40 +0100

    maj du compte rendu + improve code

--- TP2 main.py
+++ TP2 main.py
@@ -213,6 +213,8 @@ def fausse_position(fct, b_inf, b_sup, precision):
     -------
     fausse_position(b_sup=1, b_inf=2, fct=lambda x: 0.51 * x - np.sin(x))
     """
+        # Print the value of the fct parameter
+    # print(f'The value of fct is: {fct.}')
     print("\n\nCalcul de la racine avec la méthode de la fausse position") 
     precision = 10**(-4)
     n = 0
@@ -242,14 +244,26 @@ if __name__ == "__main__" :
     # il est important de définir la dérivée (dfct pour newton) 
     
     ######### Fonction / Dérivés #########
-    # fct=lambda x: 0.51 * x - np.sin(x)), dfct=
+    # fct=lambda x: x**2-2, dfct=lambda x:2*x
+    # fct=lambda x: 0.51 * x - np.sin(x)), dfct=lambda x:
     # fct=lambda x: (1 - 0.61 * x) / x, dfct=lambda x: -(1-0.61*x)/x**2-0.61/x)
-    # fct=lambda x: np.exp(x ** 2) - 56 * np.exp(-2 * x ** 2)
+    # fct=lambda x: np.exp(x ** 2) - 56 * np.exp(-2 * x ** 2), dfct=lambda x:
     
     
     ######### Partie 1 #########
-    
+    print("Question 1")
+    print("Une fonction qui a pour racine : racine de 2 peut etre :")
+    print("f(x)=x**2-2")
 
+    cordes(b_sup=1, b_inf=2, precision = 10**(-4), fct=lambda x: x**2-2)
+    print("----------------")
+    dicotomie(b_sup=1, b_inf=2, precision = 10**(-4), fct=lambda x: x**2-2)
+    print("----------------")
+    newton(val=2, precision = 10**(-4), fct=lambda x: x**2-2, dfct=lambda x:2*x)
+    print("----------------")
+    secante(b_sup=1, b_inf=2, precision = 10**(-4), fct=lambda x: x**2-2)
+    print("----------------")
+    fausse_position(b_sup=1, b_inf=2, precision = 10**(-4), fct=lambda x: x**2-2)
 
     # fausse_position(b_sup=1, b_inf=2, precision = 10**(-4), fct=lambda x: 0.51 * x - np.sin(x))
     # print("----------------")
@@ -257,4 +271,6 @@ if __name__ == "__main__" :
     # print("----------------")
     # newton(val=1, precision = 10**(-4), fct=lambda x: (1 - 0.61 * x) / x, dfct=lambda x: -(1-0.61*x)/x**2-0.61/x)
     
-    fausse_position(b_sup=1, b_inf=2, precision = 10**(-4), fct=lambda x: x**2-2)
+    # fausse_position(b_sup=1, b_inf=2, precision = 10**(-4), fct=lambda x: x**2-2)
+    
+    # secante(b_sup=1, b_inf=2, precision = 10**(-4), fct=lambda x: np.exp(x ** 2) - 56 * np.exp(-2 * x ** 2))
